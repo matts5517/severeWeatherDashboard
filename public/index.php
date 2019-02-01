df5bc3e67cf383c3f7c28d1ad149be228055ea84
Matt Silveira <msilveira@tnc.org>
2019-02-01 10:54:38 -0700

    creating file structure

---
+++ public/index.php
@@ -0,0 +1,19 @@
+
+<!-- index.php public facing site for  -->
+
+<!-- Bring in the init php functions -->
+<?php require_once('../private/initialize.php'); ?>
+
+
+<?php 
+
+?>
+
+
+<!-- start of html -->
+
+<div>
+	<li>hello li</li>
+</div>
+
+
